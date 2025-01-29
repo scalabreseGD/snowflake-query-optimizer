8a313978c6445fbdceb5eeaa0cd223aa0f941b28
Karol Laszewski <karol.laszewski@inspirebrands.com>
2025-01-29 16:30:34 +0100

    [klaszewski] Manual Analysis fix

--- src/snowflake_optimizer/utils.py
+++ src/snowflake_optimizer/utils.py
@@ -72,7 +72,8 @@ def create_results_expanders(executor: SnowflakeQueryExecutor, results: List[Out
                 display_query_comparison(
                     executor,
                     result.original_query,
-                    result.analysis.optimized_query
+                    result.analysis.optimized_query,
+                    result['filename']
                 )
 
 
@@ -211,7 +212,7 @@ def create_excel_report(batch_results: List[OutputAnalysisModel]) -> bytes:
     return excel_data
 
 
-def display_query_comparison(executor: SnowflakeQueryExecutor, original: str, optimized: str):
+def display_query_comparison(executor: SnowflakeQueryExecutor, original: str, optimized: str, filename: str):
     """Display a side-by-side comparison of original and optimized queries."""
     if not original or not optimized:
         print("Missing query for comparison!")
@@ -234,7 +235,7 @@ def display_query_comparison(executor: SnowflakeQueryExecutor, original: str, op
     result_columns = st.columns([0.1, 0.8, 0.1])
     with result_columns[1]:
         waiting_time_in_seconds = st.slider(label="Comparing timeout in seconds. 0 is no timeout", min_value=0,
-                                            max_value=3600, value=600)
+                                            max_value=3600, value=600, key=filename)
         if st.button('Compare Original and Optimized', key=hashlib.sha256(original.encode()).hexdigest()[:32]):
             with st.spinner("Comparing original and optimized queries..."):
                 original_query_df, optimized_query_df, difference_df = executor.compare_optimized_query_with_original(
